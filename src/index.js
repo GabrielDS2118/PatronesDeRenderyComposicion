1dcaf8ee5ad1c7e7e7927f221f61dca27530cff8
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2023-03-22 15:32:48 -0500

    7.Ejemplo HOC

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/index.js';
+// import App from './App/index.js';
 import './index.css';
 
+function App({ gretting, name }) {
+  return (
+    <h1>
+      !{gretting} {name}
+    </h1>
+  );
+}
+
+function withWhatever(WrappedComponent) {
+  return function componenteDeVerdad(props) {
+    return (
+      <>
+        <WrappedComponent {...props} />
+        <p>Estoy aprendiendo HOC</p>
+      </>
+    );
+  };
+}
+
+const AppWithWhatever = withWhatever(App);
+
 ReactDOM.render(
-  <App />,
+  <AppWithWhatever gretting="Buenos días" name="Maritzita" />,
   document.getElementById('root')
 );
